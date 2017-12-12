d8285eb99d22f4c67745a20ea83b3c0b0e221a0d
Rio Kurz <riotkurz@iMac13.local>
2017-12-12 07:44:06 -0600

    weather callback implemented

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-var geocodeAddress = (address) => {
+var geocodeAddress = (address, callback) => {
   const base_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 
   request({
@@ -8,13 +8,18 @@ var geocodeAddress = (address) => {
     json: true
   },(error,response, body) => {
     if (error){
-      console.log("Unable to connect to google servers.")
+      callback("Unable to connect to google servers.");
     } else if (body.status === "ZERO_RESULTS") {
-      console.log('Unable to find that address.')
+      callback('Unable to find that address.')
     } else if (body.status === "OK") {
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+      callback(undefined,{
+        address: body.results[0].formatted_address,
+        lattitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
     }
   });
 };
+
+module.exports = {geocodeAddress};
+//79b0fb413438a82ed399d052961e30e3
---
+++ weather/weather.js
@@ -0,0 +1,24 @@
+
+const request = require('request')
+
+var getWeather = (lat,lng,callback) => {
+  const api = 'https://api.darksky.net/forecast/79b0fb413438a82ed399d052961e30e3/'
+
+  request({
+    url: api + lat + ',' + lng,
+    json: true
+  },(error,response, body) => {
+    if (!error && response.statusCode == 200){
+      callback(undefined,body.currently.temperature);
+    } else {
+      callback('Unable to fetch weather.');
+    }
+  });
+}
+
+
+
+module.exports = {
+  getWeather
+}
+//https://api.darksky.net/forecast/79b0fb413438a82ed399d052961e30e3/36.1410253,-86.67118169999999
